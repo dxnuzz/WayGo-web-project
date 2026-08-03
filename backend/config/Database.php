92438b7799d76ede7f441cbcd17128ee04d59743
thushnethmini <thusharadasanayaka2003@gmail.com>
2026-08-03 21:52:59 +0530

    Update database name in Database configuration

--- backend/config/Database.php
+++ backend/config/Database.php
@@ -2,7 +2,7 @@
 class Database
 {
     private $host = "localhost";
-    private $db_name = "mydatabase";
+    private $db_name = "waygo_db";
     private $username = "root";
     private $password = ""; 
     public $conn;
